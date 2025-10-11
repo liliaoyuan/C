632172623eb6ab0b1b01b0a4aca70facaca833c4
lly <liliaoyuanob@gmail.com>
2025-10-11 23:40:53 +0000

    Merge branch 'main' of github.com:liliaoyuan/C

--- chapter9_function/q5.s
+++ chapter9_function/q5.s
@@ -1,122 +1,63 @@
 	.file	"q5.c"
+# GNU C17 (Ubuntu 13.3.0-6ubuntu2~24.04) version 13.3.0 (x86_64-linux-gnu)
+#	compiled by GNU C version 13.3.0, GMP version 6.3.0, MPFR version 4.2.1, MPC version 1.3.1, isl version isl-0.26-GMP
+
+# GGC heuristics: --param ggc-min-expand=100 --param ggc-min-heapsize=131072
+# options passed: -mtune=generic -march=x86-64 -O2 -fasynchronous-unwind-tables -fstack-protector-strong -fstack-clash-protection -fcf-protection
 	.text
+	.p2align 4
 	.globl	by_value
 	.type	by_value, @function
 by_value:
-.LFB0:
+.LFB23:  # local function begin 23
 	.cfi_startproc
-	endbr64
-	pushq	%rbp
-	.cfi_def_cfa_offset 16
-	.cfi_offset 6, -16
-	movq	%rsp, %rbp
-	.cfi_def_cfa_register 6
-	movsd	%xmm0, -8(%rbp)
-	movsd	%xmm1, -16(%rbp)
-	movsd	-8(%rbp), %xmm0
-	comisd	-16(%rbp), %xmm0
-	jbe	.L6
-	movsd	-8(%rbp), %xmm0
-	movsd	%xmm0, -16(%rbp)
-	jmp	.L7
-.L6:
-	movsd	-16(%rbp), %xmm0
-	movsd	%xmm0, -8(%rbp)
-.L7:
-	nop
-	popq	%rbp
-	.cfi_def_cfa 7, 8
-	ret
+	endbr64	
+# q5.c:10: }
+	ret	
 	.cfi_endproc
-.LFE0:
+.LFE23:  # local functionm end 23  函数作用域
 	.size	by_value, .-by_value
+	.p2align 4
 	.globl	by_pointer
 	.type	by_pointer, @function
 by_pointer:
-.LFB1:
+.LFB24:
 	.cfi_startproc
-	endbr64
-	pushq	%rbp
-	.cfi_def_cfa_offset 16
-	.cfi_offset 6, -16
-	movq	%rsp, %rbp
-	.cfi_def_cfa_register 6
-	movq	%rdi, -8(%rbp)
-	movq	%rsi, -16(%rbp)
-	movq	-8(%rbp), %rax
-	movsd	(%rax), %xmm0
-	movq	-16(%rbp), %rax
-	movsd	(%rax), %xmm1
-	comisd	%xmm1, %xmm0
-	jbe	.L13
-	movq	-8(%rbp), %rax
-	movsd	(%rax), %xmm0
-	movq	-16(%rbp), %rax
-	movsd	%xmm0, (%rax)
-	jmp	.L14
-.L13:
-	movq	-16(%rbp), %rax
-	movsd	(%rax), %xmm0
-	movq	-8(%rbp), %rax
-	movsd	%xmm0, (%rax)
-.L14:
-	nop
-	popq	%rbp
-	.cfi_def_cfa 7, 8
-	ret
+	endbr64	
+# q5.c:14:     if (*a > *b) {
+	movsd	(%rdi), %xmm1	# *a_5(D), _1
+# q5.c:14:     if (*a > *b) {
+	movsd	(%rsi), %xmm0	# *b_6(D), _2
+# q5.c:14:     if (*a > *b) {
+	comisd	%xmm0, %xmm1	# _2, _1
+	jbe	.L8	#,
+# q5.c:15:         *b = *a;
+	movsd	%xmm1, (%rsi)	# _1, *b_6(D)
+	ret	
+	.p2align 4,,10
+	.p2align 3
+.L8:
+# q5.c:17:         *a = *b;
+	movsd	%xmm0, (%rdi)	# _2, *a_5(D)
+# q5.c:19: }
+	ret	
 	.cfi_endproc
-.LFE1:
+.LFE24:
 	.size	by_pointer, .-by_pointer
+	.section	.text.startup,"ax",@progbits
+	.p2align 4
 	.globl	main
 	.type	main, @function
 main:
-.LFB2:
+.LFB25:
 	.cfi_startproc
-	endbr64
-	pushq	%rbp
-	.cfi_def_cfa_offset 16
-	.cfi_offset 6, -16
-	movq	%rsp, %rbp
-	.cfi_def_cfa_register 6
-	subq	$32, %rsp
-	movq	%fs:40, %rax
-	movq	%rax, -8(%rbp)
-	xorl	%eax, %eax
-	movsd	.LC0(%rip), %xmm0
-	movsd	%xmm0, -24(%rbp)
-	movsd	.LC1(%rip), %xmm0
-	movsd	%xmm0, -16(%rbp)
-	movsd	-16(%rbp), %xmm0
-	movq	-24(%rbp), %rax
-	movapd	%xmm0, %xmm1
-	movq	%rax, %xmm0
-	call	by_value
-	leaq	-16(%rbp), %rdx
-	leaq	-24(%rbp), %rax
-	movq	%rdx, %rsi
-	movq	%rax, %rdi
-	call	by_pointer
-	movl	$0, %eax
-	movq	-8(%rbp), %rdx
-	subq	%fs:40, %rdx
-	je	.L17
-	call	__stack_chk_fail@PLT
-.L17:
-	leave
-	.cfi_def_cfa 7, 8
-	ret
+	endbr64	
+# q5.c:26: }
+	xorl	%eax, %eax	#
+	ret	
 	.cfi_endproc
-.LFE2:
+.LFE25:
 	.size	main, .-main
-	.section	.rodata
-	.align 8
-.LC0:
-	.long	0
-	.long	1074266112
-	.align 8
-.LC1:
-	.long	0
-	.long	1075576832
 	.ident	"GCC: (Ubuntu 13.3.0-6ubuntu2~24.04) 13.3.0"
 	.section	.note.GNU-stack,"",@progbits
 	.section	.note.gnu.property,"a"
