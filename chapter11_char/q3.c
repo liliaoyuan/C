82136a50dd144a686d86ebf819832c6fe5fa52dd
lly <liliaoyuanob@gmail.com>
2025-11-27 03:31:42 +0000

    newbook

---
+++ chapter11_char/q3.c
@@ -0,0 +1,18 @@
+/*
+考察条件语句+字符串流
+*/
+
+#include<stdio.h>
+void StoreUnemptyWORD(string a[],int n){
+    int i;
+    char word[];
+    printf("please enter N number",n);
+    while(i<n):
+    scanf("%c",a[i]);
+    for(i=0;i<n;i++){
+        if (a[i])
+    }
+
+
+
+}
