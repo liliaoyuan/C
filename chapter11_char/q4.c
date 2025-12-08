05782a39a8e0b59d48f2175c0277a8b6fe6e4a72
lly <liliaoyuanob@gmail.com>
2025-12-08 09:01:40 +0000

    newq4.c

--- chapter11_char/q4.c
+++ chapter11_char/q4.c
@@ -1,10 +1,10 @@
 /*
-scanf接收*/
+scanf"c%"会接收因为键盘输入的回车，即翻译成\n scanf" %c"会跳过空白字符
+*/
 
 #include<stdio.h>
 
-#define maxnumber 10 
-void StoreUnemptyWORD(char a[],int n){
+void StoreUnemptyWORD(char a[],int maxnumber,int n){
     int i=0;
     char word[maxnumber];
     while(i<maxnumber){
@@ -22,9 +22,11 @@ void StoreUnemptyWORD(char a[],int n){
 
 void main(){
     int i ;
-    int n;
+    int n,maxnumber;
     printf("enter the character length you want\n");
     scanf("%d",&n);
+    printf("enter the max read numbner you wnat");
+    scanf("%d",&maxnumber);
     char arr1[n];
     StoreUnemptyWORD(arr1,n);
     for (i=0;i<7;i++){
